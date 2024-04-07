4b10d707e3857ff18d9656667c1f1f4df11d3565
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-04-07 11:55:09 +0900

    init: dependency, application.yml 초기 설정

---
+++ src/main/java/com/hanwha/solbangulrest/SolbangulRestApplication.java
@@ -0,0 +1,13 @@
+package com.hanwha.solbangulrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolbangulRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolbangulRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanwha/solbangulrest/SolbangulRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanwha.solbangulrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolbangulRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
