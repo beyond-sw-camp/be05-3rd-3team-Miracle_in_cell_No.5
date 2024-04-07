2ba39e8df53dd5bd7eecd1b739658f3324b224b2
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-04-07 22:26:04 +0900

    feat: BaseTimeEntity - 생성 시간, 수정 시간, JpaAuditing 기능 추가

--- src/main/java/com/hanwha/solbangulrest/SolbangulRestApplication.java
+++ src/main/java/com/hanwha/solbangulrest/SolbangulRestApplication.java
@@ -2,12 +2,14 @@ package com.hanwha.solbangulrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SolbangulRestApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SolbangulRestApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SolbangulRestApplication.class, args);
+	}
 
 }
