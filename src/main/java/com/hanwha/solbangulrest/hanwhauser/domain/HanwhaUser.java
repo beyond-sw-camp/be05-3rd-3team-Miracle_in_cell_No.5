cf70f509a4c3e8305f18360f5810d40e06012907
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-04-11 15:30:34 +0900

    fix: HanwhaUser 엔티티 @Table 추가, JoinServiceTest 삭제

--- src/main/java/com/hanwha/solbangulrest/hanwhauser/domain/HanwhaUser.java
+++ src/main/java/com/hanwha/solbangulrest/hanwhauser/domain/HanwhaUser.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import org.hibernate.annotations.ColumnDefault;
 
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Table(name = "hanwha_user")
 public class HanwhaUser {
 
 	@Id
