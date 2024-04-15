f4d7035c298d9e9e93ca4f2101807a16248e7226
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-04-15 17:43:11 +0900

    chore: 백엔드, 프론트엔드 분리

--- backend/src/main/java/com/hanwha/solbangulrest/speaker/controller/SpeakerController.java
+++ backend/src/main/java/com/hanwha/solbangulrest/speaker/controller/SpeakerController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hanwha.solbangulrest.speaker.dto.SpeakerDto;
@@ -16,7 +17,8 @@ import com.hanwha.solbangulrest.speaker.service.SpeakerService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@RestController("/speakers")
+@RequestMapping("/speakers")
+@RestController
 public class SpeakerController {
 
 	private final SpeakerService speakerService;
