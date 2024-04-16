7b4cac80879ca4cf1e10af720c86b0fcfd1e98db
chaewon02~ <glgl246@naver.com>
2024-04-16 14:50:15 +0900

    create front

---
+++ frontend/src/components/AppFooter.vue
@@ -0,0 +1,39 @@
+<template>
+    <div className="Footer-container">
+            <div className='footer-contact'>
+                ⓒ 한화 시스템 SW Camp 5th
+            </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.Footer-container {
+    background-color: #f8f9fa;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    padding: 20px 0;
+    margin-bottom: 0px;
+}
+.footer-logo {
+    font-size: 18px;
+    font-weight: 700;
+}
+.logo-gray {
+    width: 80px;
+    height: auto;   
+}
+.footer-contact {
+    margin-top: 5px;
+    font-size: 14px;
+    font-size: 600;
+}
+</style>
