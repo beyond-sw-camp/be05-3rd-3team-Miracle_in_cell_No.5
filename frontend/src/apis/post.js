0895975d763ad1c5f8a585ecd388d0013cd2199d
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-04-17 21:02:43 +0900

    feat: axios 헤더에 token 추가

--- frontend/src/apis/post.js
+++ frontend/src/apis/post.js
@@ -1,19 +1,48 @@
-import axios from './config' ;
+import axios from './config';
 
 export default {
-    savePostById(roomId,data) {
-        return axios.post(`/posts/${roomId}`, data);
+    savePostById(roomId, data) {
+        return axios.post(`/posts/${roomId}`, data, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem('token')
+                }
+            }
+        );
     },
-    patchPostById(postId,data) {
-        return axios.patch(`/posts/${postId}`,data) ;
+    patchPostById(postId, data) {
+        return axios.patch(`/posts/${postId}`, data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')
+            }
+        });
     },
-    deletePostById(postId){
-        return axios.delete(`/posts/${postId}`) ;
+    deletePostById(postId) {
+        return axios.delete(`/posts/${postId}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem('token')
+                }
+            }
+        );
     },
-    getPostById(postId){
-        return axios.get(`/posts/${postId}`) ;
+    getPostById(postId) {
+        return axios.get(`/posts/${postId}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem('token')
+                }
+            }
+        );
     },
-    getPostsByRoomId(roomId,keyword,category){
-        return axios.get(`/room/${roomId}/search`,keyword,category);
+    getPostsByRoomId(roomId, keyword, category) {
+        return axios.get(`/room/${roomId}/search`, keyword, category, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem('token')
+                }
+            }
+        );
     }
 };
